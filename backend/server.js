ad038c9c2ded5132c919c29ac368abb55cb17801
youssef-dawod <youssef-dawod>
2024-10-09 15:47:23 +0200

    client erstellt

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,36 @@
-/* Import the Express module */
+// Import the necessary modules
 const express = require("express");
-/* Create an instance of an Express application  */
+const cors = require("cors");
+require('dotenv').config();
+
+// Create an instance of an Express application
 const app = express();
-/*Mount App1 */
+
+// Middleware
+app.use(cors({
+  origin: 'http://localhost:5173', // Port von Vite
+  credentials: true,
+}));
+app.use(express.json());
+
+// Mount App1
 const app1 = require("./App1/User.js");
-/* Mounts App1 at the '/app1' endpoint  */
 app.use("/app1", app1);
-/*  Mount App2 */
+
+// Mount App2
 const app2 = require("./App2/Models.js");
-/*  Mounts App2 at the '/app2' endpoint  */
 app.use("/app2", app2);
-/*  Mount App3 */
+
+// Mount App3
 const app3 = require("./App3/Text.js");
-/*  Mounts App3 at the '/app3' endpoint  */
 app.use("/app3", app3);
-/*  Mount App4 */
+
+// Mount App4
 const app4 = require("./App4/Image.js");
-/*  Mounts App4 at the '/app4' endpoint  */
 app.use("/app4", app4);
-/* Start the server */
-const port = 5000;
+
+// Start the server
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`CombinedApp is listening on port ${port}`);
 });
