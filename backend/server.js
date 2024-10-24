84a59388f4cb416f60fe0a9ff8ade1a622994053
youssef-dawod <youssef-dawod>
2024-10-24 08:54:58 +0200

    Resolved merge conflict in server.js

--- backend/server.js
+++ backend/server.js
@@ -15,32 +15,16 @@ app.use(express.json());
 
 // Mount App1
 const app1 = require("./App1/User.js");
-<<<<<<< HEAD
-=======
 /* Mounts App1 at the '/app1' endpoint / Not used in browser*/
->>>>>>> f59a70118850a967e3617a236db1255940312088
 app.use("/app1", app1);
 
 // Mount App2
 const app2 = require("./App2/Models.js");
-<<<<<<< HEAD
-=======
 /*  Mounts App2 at the '/app2' endpoint  / Not used in browser*/
->>>>>>> f59a70118850a967e3617a236db1255940312088
 app.use("/app2", app2);
 
 // Mount App3
 const app3 = require("./App3/Text.js");
-<<<<<<< HEAD
-app.use("/app3", app3);
-
-// Mount App4
-const app4 = require("./App4/Image.js");
-app.use("/app4", app4);
-
-// Start the server
-const port = process.env.PORT || 5000;
-=======
 /*  Mounts App3 at the '/text' endpoint  */
 app.use("/api/", app3);
 /*  Mount App4 */
@@ -49,7 +33,6 @@ const app4 = require("./App4/Straico-Image.js");
 app.use("/api/image", app4);
 /* Start the server */
 const port = 5000;
->>>>>>> f59a70118850a967e3617a236db1255940312088
 app.listen(port, () => {
   console.log(`CombinedApp is listening on port ${port}`);
 });
