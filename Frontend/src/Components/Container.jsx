8f34c8c8a51376ffa3c55e3e5beb111a3e446a27
Mario-Laudi <mario.laudi@dci-student.org>
2024-10-29 09:11:37 +0100

    321

---
+++ Frontend/src/Components/Container.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "./Container.css";
+
+function Container() {
+  return (
+    <div className="container">
+      <p>
+        Die einzelnen Urnenreihen, oder Flächen sind jeweils durch mindestens 2
+        <br />
+        Markierungspfähle gekennzeichnet, so dass ein
+        <br />
+        Aussuchen und genaues vermessen der Grabstelle möglich ist.
+        <br /> <br />
+        Das Team vom Goldenbeker Urnengartenbehält sich vor, einen
+        <br />
+        Wunschplatz geringfügig zu verlegen, falls sich dort Baumwurzeln
+        befinden,
+        <br />
+        oder ein anderer Grund vorliegt.
+        <br />
+        Es besteht kein Rechtsanspruch auf Platz oder Durchführung.
+        <br />
+        <br />
+        Um eine ordnungsgemäße Waldbewirtschaftung zu garantieren,
+        <br />
+        gibt es für die Urnenplätze keine Baumzuordnung,
+        <br />
+        sondern
+        <br />
+       
+      </p>
+    </div>
+  );
+}
+
+export default Container;
